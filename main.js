d95ea1343959e1a05d856c0e4570a5ce9b7c00e7
hiroshi <halbrot1@gmail.com>
2021-07-23 15:40:59 +0900

    特性変更時にsinglePlotも更新

--- main.js
+++ main.js
@@ -1,49 +1,50 @@
+let fontsize = 18;
+let axisSetting = {
+  tickmode: 'auto',
+  ticks: 'inside',
+  linewidth: 1,
+  mirror: true,
+  titlefont: {size: fontsize} ,
+  tickfont: {size: fontsize}
+}
+
 var layout = {
   // title:'IPQ',
   height: 600,
   xaxis: {
-    tickmode: 'auto',
-    ticks: 'inside',
-    linewidth: 1,
-    mirror: true,
     title: '実施日時',
+    ...axisSetting
   },
   yaxis: {
-    tickmode: 'auto',
-    ticks: 'inside',
-    linewidth: 1,
-    mirror: true,
-    title: '温度 (℃)'
+    title: '温度 (℃)',
+    ...axisSetting
   },
   yaxis2: {
     overlaying: 'y',
     side: 'right',
     autorange: 'reversed',
-    }
+    ...axisSetting
+  }
   // width: 600
 };
 
 
 var layout2 = {
   xaxis: {
-    tickmode: 'auto',
-    ticks: 'inside',
-    linewidth: 1,
-    mirror: true,
     title: '加熱時間 (s)',
+    ...axisSetting
   },
   yaxis: {
-    tickmode: 'auto',
-    ticks: 'inside',
-    linewidth: 1,
-    mirror: true,
-    title: '温度 (℃)'},
+    title: '温度 (℃)',
+    ...axisSetting
+  },
   // title: 'single graph',
-
   yaxis2: {
     title: 'MV',
     overlaying: 'y',
-    side: 'right'
+    side: 'right', 
+    ...axisSetting
+
   },
   height: 600,
   // width: 600
@@ -78,6 +79,7 @@ var app = new Vue({
     ambfiles: null,
     ambforplot: null,
     selectedCharacter: 'temperature',
+    loading: false,
     characterList: [
       {text: '炭化物面積率', value: 'carbide'},
       {text: '最高到達温度', value: 'temperature'},
@@ -101,8 +103,7 @@ var app = new Vue({
       {text: '2021/6 GRW5102(B0)', value: 'Z:/01_研究テーマ/14_三重IH改善/08_生産チャート/202106_GRW5102B0/'},
       {text: '2021/7 GRW5102(B0)', value: 'Z:/01_研究テーマ/14_三重IH改善/08_生産チャート/202107_GRW5102B0/'},
       {text: '2021/7 GRW5102(C0)', value: 'Z:/01_研究テーマ/14_三重IH改善/08_生産チャート/202107_GRW5102C0/'}
-    ],
-    loading: false,
+    ]
   },
   methods: {
     historyplot: function(refresh){
@@ -110,7 +111,7 @@ var app = new Vue({
       this.loading = true;
       axios
         .get(`http://10.112.120.156:5000/getdata?path=${this.path}&character=${this.selectedCharacter}&startsec=${this.startSec}&endsec=${this.endSec}&refresh=${refresh}`)
-        .then(response => {
+        .then((response) => {
 
           // オブジェクトの要素数を取得
           // 日時，id は必ず含まれており，それ以外が特性値
@@ -119,12 +120,12 @@ var app = new Vue({
           x = response.data.datetime;
           id = response.data.id;
 
-          seriesname = ['外側', '内側']
-          y = []
-          series = []
+          seriesname = ['外側', '内側'];
+          y = [];
+          series = [];
           for(let i=0;i<num-2;i++) {
             y.push(response.data['data' + String(i)]);
-            series.push({x:x, y:y[i], text:id, name:seriesname[i], mode: 'markers', type: 'scatter', marker: { size: 6 }})
+            series.push({x:x, y:y[i], text:id, name:seriesname[i], mode: 'markers', type: 'scatter', marker: { size: 6 }});
           }
     
           this.loading = false;
@@ -135,7 +136,39 @@ var app = new Vue({
             app.id = parseInt(data.points[0].text);
           });
         })
+      
       this.ambCheck()
+    },
+    singlePlot: function(){
+      axios
+        .get(`http://10.112.120.156:5000/singleplot?path=${this.path}&character=${this.selectedCharacter}&id=${this.id}`)
+        .then(response => {
+
+        // オブジェクトの要素数を取得
+        // 日時，id は必ず含まれており，それ以外が特性値
+        // このため，特性値の数は num-2
+        num = Object.keys(response.data).length;
+        x = response.data.datetime;
+        mv = response.data.mv;
+
+        if (num===4){
+          seriesname = ['外側', '内側']
+        }
+        else{
+          seriesname = [this.selectedCharacterl]
+        }
+
+        y = []
+        series = []
+        for(let i=0;i<num-2;i++) {
+          y.push(response.data['data' + String(i)]);
+          series.push({x:x, y:y[i], text:id, name:seriesname[i], mode: 'lines', type: 'scatter', marker: { size: 6 }})
+        }
+
+        if (this.drawMV) series.push({x:x, y:mv, text:id, name:'MV',yaxis: 'y2', mode: 'lines', type: 'scatter', marker: { size: 6 }});
+
+        Plotly.newPlot('singlePlot', series, layout2, config)
+        });
     },
     ambCheck: function(){
 
@@ -177,58 +210,26 @@ var app = new Vue({
     }
   },
   watch: {
-    path:{
-      handler: function() {
-        this.historyplot(0)
-      }
+    path() {
+      this.historyplot(0)
     },
-    selectedCharacter:{
-      handler: function(){
-        layout.yaxis.title = labelDic[this.selectedCharacter];
-        layout2.yaxis.title = labelDic[this.selectedCharacter];
-        this.historyplot(0);
-        this.judgePower();
-        
-      }
+    selectedCharacter() {
+      layout.yaxis.title = labelDic[this.selectedCharacter];
+      layout2.yaxis.title = labelDic[this.selectedCharacter];
+      this.historyplot(0);
+      this.singlePlot();
+      this.judgePower();
     },
-    id:{
-      handler: function(){
-        axios
-          .get(`http://10.112.120.156:5000/singleplot?path=${this.path}&character=${this.selectedCharacter}&id=${this.id}`)
-          .then(response => {
-
-          // オブジェクトの要素数を取得
-          // 日時，id は必ず含まれており，それ以外が特性値
-          // このため，特性値の数は num-2
-          num = Object.keys(response.data).length;
-          x = response.data.time;
-          mv = response.data.mv;
-
-          if (num===4){
-            seriesname = ['外側', '内側']
-          }
-          else{
-            seriesname = [this.selectedCharacterl]
-          }
-
-          y = []
-          series = []
-          for(let i=0;i<num-2;i++) {
-            y.push(response.data['data' + String(i)]);
-            series.push({x:x, y:y[i], text:id, name:seriesname[i], mode: 'lines', type: 'scatter', marker: { size: 6 }})
-          }
-
-          if (this.drawMV) series.push({x:x, y:mv, text:id, name:'MV',yaxis: 'y2', mode: 'lines', type: 'scatter', marker: { size: 6 }});
-
-          Plotly.newPlot('singlePlot', series, layout2, config)
-          });
-      }
+    id() {
+      this.singlePlot()
     },
-    ambforplot:{
-      handler: function(){
-        this.amb();
-      }
+    ambforplot() {
+      this.amb();
     }
+
+    //深い階層の場合は書き方が異なる
+    //https://qiita.com/shizen-shin/items/ef20d7b9649234ba26ae
+
   }
 
 });
